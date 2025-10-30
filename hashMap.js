d72f4d305b722bb1963d0329335f0f8ed2cefd87
fidel208 <dempafidel@gmail.com>
2025-10-30 19:13:33 +0300

    Almost done,,, testing is remaining

---
+++ hashMap.js
@@ -0,0 +1,126 @@
+export class hashMap {
+  constructor(capacity = 16, loadFactor = 0.75) {
+    this.capacity = capacity;
+    this.loadFactor = loadFactor;
+    this.size = 0;
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+  }
+
+  _hash(key) {
+    let hashCode = 0;
+
+    const primeNUmber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNUmber * hashCode + key.charCodeAt(i);
+    }
+    return hashCode % this.capacity;
+  }
+
+  set(key, value) {
+    const index = this._hash(key);
+    const bucket = this.buckets[index];
+
+    for (let pair of bucket) {
+      if (pair[0] === key) {
+        pair[1] = value;
+        return;
+      }
+    }
+    bucket.push([key, value]);
+    this.size++;
+
+    if (this.size / this.capacity > this.loadFactor) {
+      this._resize();
+    }
+  }
+
+  get(key) {
+    const index = this._hash(key);
+    const bucket = this.buckets[index];
+
+    for (let pair of bucket) {
+      if (pair[0] === key) {
+        return pair[1];
+      }
+    }
+    return null;
+  }
+
+  has(key) {
+    const index = this._hash(key);
+    const bucket = this.buckets[index];
+
+    for (let pair of bucket) {
+      if (pair[0] === key) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  remove(key) {
+    const index = this._hash(key);
+    const bucket = this.buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] == key) {
+        bucket.splice(i, 1);
+        this.size--;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  length() {
+    return this.size;
+  }
+
+  clear() {
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+    this.size = 0;
+  }
+
+  keys() {
+    const keysArray = [];
+    for (let bucket of this.buckets) {
+      for (let [key, _] of bucket) {
+        keysArray.push(key);
+      }
+    }
+    return keysArray;
+  }
+
+  values() {
+    const valuesArray = [];
+    for (let bucket of this.buckets) {
+      for (let [_, value] of bucket) {
+        valuesArray.push(value);
+      }
+    }
+    return valuesArray;
+  }
+
+  entries() {
+    const entriesArray = [];
+    for (let bucket of this.buckets) {
+      for (let pair of bucket) {
+        entriesArray.push(pair);
+      }
+    }
+    return entriesArray;
+  }
+
+  _resize() {
+    const oldBUckets = this.buckets;
+    this.capacity *= 2;
+    this.size = 0;
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+
+    for (let bucket of oldBUckets) {
+      for (let [key, value] of buckets) {
+        this.set(key, value);
+      }
+    }
+  }
+}
